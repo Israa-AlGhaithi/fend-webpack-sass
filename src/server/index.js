d78bd40badd8e7078f12c89113fc7b9a623e3f38
Israa Al-Ghaithi <issralghaithi@hotmail.com>
2024-03-01 03:39:06 +0400

    In this branch I worked on 'Fixing Our Functionality'     modified:   package.json line 9 to use webpack-dev-server 	modified:   src/client/index.js add lines 12 to 15, the export statement 	modified:   src/client/js/formHandler.js:     line 6: to confirm that the javascript function calls refer to the Client library     line 9: Change the port number in the fetch request from 8080 to 8081
    	modified:   src/client/styles/footer.scss add the styles for the footer, lines 1 to 5
    	modified:   src/client/views/index.html:
        line 28 to confirm that the custom 'handleSubmit()' function references the newly created Client library
    
    	modified:   src/server/index.js: add lines 11 to 15,
        Because of the browser's console error related to Cross-Origin Resource Sharing (CORS)
    
    	modified:   webpack.dev.js add lines 10-13 for the output section
    	modified:   webpack.prod.js add lines 8-11 for the output section | add scss test in lines 18-22

--- src/server/index.js
+++ src/server/index.js
@@ -8,13 +8,20 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+// Allow requests from http://localhost:8080
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    next();
+  });
+
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
