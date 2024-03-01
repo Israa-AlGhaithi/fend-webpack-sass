83e766951ef451ffd52c86d7f4df08c1507cdeeb
Israa Al-Ghaithi <issralghaithi@hotmail.com>
2024-03-01 06:07:47 +0400

    A branch for the API call challenge task.. about implementing a fetch request to an external API and get the response to show up in the view of your app.     modified:   src/client/js/formHandler.js :     lines 7-12: get the zip code input, declare the variables for the api credentials key (OpenWeatherMaps website). 	line 15: change the fetch url from localhost server to an external api url.     lines 18-23: add them to show the results of api response in the webpage.
        modified:   src/client/views/index.html :
        line 27: delete the method 'onblur'.
        line 28: add it for the zip Code.
        lines 35-39: add them as a containers for the api response.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,12 +4,24 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
+    const zipCode = document.getElementById('zip').value;
+
+    /* Variables */
+    const baseURL = 'https://api.openweathermap.org/data/2.5/';
+    const apiKey = 'b4c921d8c7cd365351d4d3b006368d9d&units=imperial'; //my api key
+    const baseIconURL = 'https://openweathermap.org/img/wn/';
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(`${baseURL}weather?zip=${zipCode}&appid=${apiKey}`)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        const icon = res.weather[0].icon;
+        document.getElementById('results-name').innerHTML = `Name: ${formText}`;
+        document.getElementById('results-zip').innerHTML = `Zip Code: ${zipCode}`;
+        document.getElementById('results-temp').innerHTML = `Temperature: ${Math.round(res.main.temp)}  °F `;
+        document.getElementById('results-weather').innerHTML = `Weather Status: ${res.weather[0].main}`;
+        document.getElementById('results-icon').innerHTML = ` <img src='${baseIconURL}${icon}@4x.png' alt='weather icon'> `;
+
     })
 }
 
